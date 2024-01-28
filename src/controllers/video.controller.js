9480836df20dcbe2c4878df65758cf50fff23781
Adarsh Kaushal <adarshkaushal14@gmail.com>
2024-01-28 14:44:09 +0530

    half video controller completed

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -1,98 +1,160 @@
-import mongoose, {isValidObjectId} from "mongoose"
-import {Video} from "../models/video.model.js"
-import {User} from "../models/user.model.js"
-import {ApiError} from "../utils/ApiError.js"
-import {ApiResponse} from "../utils/ApiResponse.js"
-import {asyncHandler} from "../utils/asyncHandler.js"
-import {uploadOnCloudinary} from "../utils/cloudinary.js"
+import mongoose, { isValidObjectId, trusted } from "mongoose"
+import { Video } from "../models/video.model.js"
+import { User } from "../models/user.model.js"
+import { ApiError } from "../utils/ApiError.js"
+import { ApiResponse } from "../utils/ApiResponse.js"
+import { asyncHandler } from "../utils/asyncHandler.js"
+import { uploadOnCloudinary } from "../utils/cloudinary.js"
 
 
 const getAllVideos = asyncHandler(async (req, res) => {
     const { page = 1, limit = 10, query, sortBy, sortType, userId } = req.query
     //TODO: get all videos based on query, sort, pagination
+    
 })
 
 const publishAVideo = asyncHandler(async (req, res) => {
-    const { title, description} = req.body
+    const { title, description } = req.body
     // TODO: get video, upload to cloudinary, create video
-    if(title.trim() === "")
-    {
-        throw new ApiError(401 , "No title given.")
+    if (title.trim() === "") {
+        throw new ApiError(401, "No title given.")
     }
 
     const user = req.user?._id;
 
-    if(!req.files.videoFile || !req.files.thumbnail)
-    {
-        throw new ApiError(401 , "Video file or thumbnail not found.")
+    if (!req.files.videoFile || !req.files.thumbnail) {
+        throw new ApiError(401, "Video file or thumbnail not found.")
     }
 
     const video = await uploadOnCloudinary(req.files?.videoFile[0].path)
     const thumbnail = await uploadOnCloudinary(req.files?.thumbnail[0].path)
-    
-    if(!video || !thumbnail)
-    {
-        throw new ApiError(500 , "Error while uploading video.")
+
+    if (!video || !thumbnail) {
+        throw new ApiError(500, "Error while uploading video.")
     }
 
     const videoModel = await Video.create({
-        duration : video.duration,
-        videoFile : video.url,
-        thumbnail : thumbnail.url,
+        duration: video.duration,
+        videoFile: video.url,
+        thumbnail: thumbnail.url,
         description,
-        views : 0,
-        owner : user
+        views: 0,
+        owner: user
     })
 
-    if(!videoModel)
-    {
-        throw new ApiError(500 , "Error while uploading video.")
+    if (!videoModel) {
+        throw new ApiError(500, "Error while uploading video.")
     }
 
-    return res.status(200).json(new ApiResponse(200 , videoModel , "video uploaded successfully."))
+    return res.status(200).json(new ApiResponse(200, videoModel, "video uploaded successfully."))
 })
 
 const getVideoById = asyncHandler(async (req, res) => {
     const { videoId } = req.params
     //TODO: get video by id
-    if(!isValidObjectId(videoId))
-    {
-        throw new ApiError(401 , "Not valid video id.")
+    if (!isValidObjectId(videoId)) {
+        throw new ApiError(401, "Not valid video id.")
     }
     const video = await Video.findById(videoId);
 
-    if(!video)
-    {
-        throw new ApiError(500 , "Some error occured while finding video.")
+    if (!video) {
+        throw new ApiError(500, "Some error occured while finding video.")
     }
 
-    return res.status(200).json(new ApiResponse(200 , video , "Video found successfully."))
+    return res.status(200).json(new ApiResponse(200, video, "Video found successfully."))
 
 })
 
 const updateVideo = asyncHandler(async (req, res) => {
+
     const { videoId } = req.params
     //TODO: update video details like title, description, thumbnail
-    const {title , description , thumbnail} = req.body;
+    const { title, description } = req.body;
     const user = req.user?._id;
     const video = await Video.findById(videoId);
 
-    if(video.owner.toString !== user.toString())
-    {
-        throw new ApiError(401 , "Unauthorized person cant update video.")
+    if (video.owner.toString !== user.toString()) {
+        throw new ApiError(401, "Unauthorized person cant update video.")
+    }
+    const thumbnail = uploadOnCloudinary(req.file?.thumbnail[0]?.path);
+
+    if (!thumbnail) {
+        thumbnail = { url: video.thumbnail };
     }
 
-    await Video.findb
+    const updatedVideo = await Video.findByIdAndUpdate(videoId,
+        {
+            $set: {
+                title,
+                description,
+                thumbnail: thumbnail.url,
+            },
+        },
+        {
+            new: true,
+        }
+    )
 
+    if (!updateVideo) {
+        throw new ApiError(500, "Some error while updating video details.")
+    }
+
+    return res.status(200).json(new ApiResponse(200, updatedVideo, "Video details updated successfully."))
 })
 
 const deleteVideo = asyncHandler(async (req, res) => {
     const { videoId } = req.params
     //TODO: delete video
+    if (!isValidObjectId(videoId)) {
+        throw new ApiError(401, "Invalid parameter while deleting video.")
+    }
+
+    const user = req.user?._id;
+    const video = await Video.findById(videoId);
+
+    if (user.toString() !== video.owner.toString()) {
+        throw new ApiError(402, "Unauthorized user trying to delete video.")
+    }
+
+    const deletedVideo = await Video.findByIdAndDelete(videoId);
+
+    if (!deletedVideo) {
+        throw new ApiError(500, "Error occured while deleting video.")
+    }
+
+    return res.status(200).json(new ApiResponse(200, deletedVideo, "Video deleted Successfully."))
 })
 
 const togglePublishStatus = asyncHandler(async (req, res) => {
     const { videoId } = req.params
+
+    if (!isValidObjectId(videoId)) {
+        throw new ApiError(401, "Invalid parameter while changing video status.")
+    }
+
+    const user = req.user?._id;
+    const video = await Video.findById(videoId);
+
+    if (user.toString() !== video.owner.toString()) {
+        throw new ApiError(402, "Unauthorized user trying to change video status.")
+    }
+
+    const updateVideoStatus = await Video.findByIdAndUpdate(videoId,
+        {
+            $set: {
+                isPublished: true ? false : true,
+            }
+        },
+        {
+            new: true,
+        })
+
+        if(!updateVideoStatus)
+        {
+            throw new ApiError(500 , "Error occured while updating video status.")
+        }
+
+        return res.status(200).json(new ApiResponse(200 , updateVideoStatus , "Video status updated."))
 })
 
 export {
